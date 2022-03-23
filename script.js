268ad538578f19dbefe5ce2ce471c931099e3de4
kanjanaw <kanjanaw00@gmail.com>
2022-03-24 01:34:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const tag3Ds = document.querySelectorAll(".card-3d")
 const tagWebs = document.querySelectorAll(".card-web")
 const tagDesigns = document.querySelectorAll(".card-design")
+const idAll = document.getElementById("all")
+const id3d = document.getElementById("3d")
+const idWeb = document.getElementById("web")
+const idDesign = document.getElementById("design")
+const idContact = document.getElementById("contact")
 
 function showAll() {
     tag3Ds.forEach(tag3D => tag3D.style.display = 'inline');
@@ -24,4 +29,13 @@ function showDesign(){
     tag3Ds.forEach(tag3D => tag3D.style.display = 'none');
     tagWebs.forEach(tagWeb => tagWeb.style.display = 'none');
     tagDesigns.forEach(tagDesign => tagDesign.style.display = 'inline');
+}
+
+var btns = document.getElementsByClassName("nav-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("item-active");
+  current[0].className = current[0].className.replace(" item-active", "");
+  this.className += " item-active";
+  });
 }
